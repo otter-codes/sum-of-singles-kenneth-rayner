ec1200ac8b316a07336a5a62ec27ec67984bbc8a
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-04-18 12:32:09 +0100

    add code and TDD

---
+++ src/main/scala/SumOfSingles.scala
@@ -0,0 +1,24 @@
+object SumOfSingles {
+
+  def sumInt(input: List[Int]): Int = {
+    input.sum
+  }
+
+  def toDistinct(input: List[Int]): List[Int] = {
+    input.distinct
+  }
+
+  def diffInputAndDistinct(input: List[Int]): List[Int] = {
+
+    input.diff(toDistinct(input))
+  }
+
+  def showUniqueNumbers(input: List[Int]): List[Int] = {
+
+    toDistinct(input).diff(diffInputAndDistinct(input))
+  }
+
+  def sumUniqueNumbers(input: List[Int]): Int = {
+    toDistinct(input).diff(diffInputAndDistinct(input)).sum
+  }
+}
